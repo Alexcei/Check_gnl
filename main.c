55766c84c4351975b953ea7997c2c27b19b483aa
bpole <bpole@student.21-school.ru>
2019-10-04 21:09:57 +0300

    main

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-/* ************************************************************************** */
+* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
@@ -6,15 +6,12 @@
 /*   By: bpole <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/27 14:29:45 by bpole             #+#    #+#             */
-/*   Updated: 2019/10/03 20:32:07 by bpole            ###   ########.fr       */
+/*   Updated: 2019/10/04 21:08:12 by bpole            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
-
-
-
 
 int		main(void)
 {
@@ -29,8 +26,8 @@ int		main(void)
     printf("%s\n", line);
 
     fd2 = open("text2.txt", O_RDONLY);
-    while (get_next_line(fd2, &line) > 0)
-        printf("%s\n", line);
+    get_next_line(fd2, &line);
+    printf("%s\n", line);
 
     get_next_line(0, &line);
     printf("%s\n", line);
@@ -42,19 +39,27 @@ int		main(void)
     fd4 = open("text4.txt", O_RDONLY);
     while (get_next_line(fd4, &line) > 0)
         printf("%s\n", line);
-    printf("%s\n", line);
+    get_next_line(fd2, &line);
+	printf("%s\n", line);
 
     printf("%d\n", get_next_line(999, &line));
     printf("%d\n", get_next_line(-1, &line));
 	return (0);
 }
 
-/*
 1234512345
-1234512345qwert
+qwert
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bpole <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/27 12:05:26 by bpole             #+#    #+#             */
+/*   Updated: 2019/09/27 15:14:39 by bpole            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 yuiop
- v and m
- tabl
- -1
- -1
- */
+-1
+-1

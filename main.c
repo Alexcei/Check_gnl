a74b27cf95db1e39daf07998476e724524902e98
bpole <bpole@student.21-school.ru>
2019-10-16 14:13:09 +0300

    main.c

--- main.c
+++ main.c
@@ -6,11 +6,12 @@
 /*   By: bpole <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/27 14:29:45 by bpole             #+#    #+#             */
-/*   Updated: 2019/10/14 22:09:29 by bpole            ###   ########.fr       */
+/*   Updated: 2019/10/16 14:10:44 by bpole            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <fcntl.h>
 #include <stdio.h>
 
 int		main(void)
@@ -24,23 +25,31 @@ int		main(void)
     fd = open("text.txt", O_RDONLY);
     get_next_line(fd, &line);
     printf("%s\n", line);
+	free(line);
 
     fd2 = open("text2.txt", O_RDONLY);
     get_next_line(fd2, &line);
     printf("%s\n", line);
+	free(line);
 
     get_next_line(0, &line);
     printf("%s\n", line);
+	free(line);
 
     fd3 = open("text3.txt", O_RDONLY);
-    while (get_next_line(fd3, &line) > 0)
+    while (get_next_line(fd3, &line) > 0) {
         printf("%s\n", line);
+		free(line);
+	}
 
     fd4 = open("text4.txt", O_RDONLY);
-    while (get_next_line(fd4, &line) > 0)
+    while (get_next_line(fd4, &line) > 0) {
         printf("%s\n", line);
+		free(line);
+	}
     get_next_line(fd2, &line);
 	printf("%s\n", line);
+	free(line);
 
     printf("%d\n", get_next_line(999, &line));
     printf("%d\n", get_next_line(-1, &line));
